ef1076a4bfd8b13d42ccd7030a45800f0959175a
Awais <m.awaisraz786@gmail.com>
2025-05-28 22:46:24 +0500

    adding second test

--- test/Token.js
+++ test/Token.js
@@ -1,15 +1,10 @@
-//const { expect } = require("chai");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-
-//const { expect } = require("chai");
-//const { ethers } = require("hardhat");
-
 describe("Token contract", function () {
+    
     it("Deployment should assign the initial supply of the tokens to the owner", async function () {
         const [owner] = await ethers.getSigners(); // to get the owner address
-
         console.log("Signers object:", owner);     // print owner address
         const Token = await ethers.getContractFactory("Token");  // compiles and load token contract
         const hardhatToken = await Token.deploy("Vending Machine", "VM", 10000);  //constructor arguments
@@ -18,25 +13,21 @@ describe("Token contract", function () {
         console.log("Owner Address:", owner.address);   // print owner address
         expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
     });
+
+    it("Should transfer tokens between accounts", async function () {
+        const [owner, addr1, addr2] = await ethers.getSigners(); // to get the owner address
+        const Token = await ethers.getContractFactory("Token");  // compiles and load token contract
+        const hardhatToken = await Token.deploy("Vending Machine", "VM", 10000);  //constructor arguments
+        await hardhatToken.deployed(); // token deployed
+        const ownerBalance = await hardhatToken.balanceOf(owner.address);
+        // transfer tokens from owner to addr1
+        await hardhatToken.transfer(addr1.address, 10);
+        expect(await hardhatToken.balanceOf(addr1.address)).to.equal(10);
+        //transfer 5 tokens from addr1 to addr2
+        await hardhatToken.connect(addr1).transfer(addr2.address, 5);
+        expect(await hardhatToken.balanceOf(addr2.address)).to.equal(5);
+    });
 });
 
 
 
-//const { expect } = require("chai");
-//const { ethers } = require("hardhat");
-
-//escribe("Token contract", function () {
-//    it("Deployment should assign the initial supply of the tokens to the owner", async function () {
-//        const [owner] = await ethers.getSigners();
-//        console.log("Signers object:", owner);
-
-//        const Token = await ethers.getContractFactory("Token");
-  //      const hardhatToken = await Token.deploy("Vending Machine", "VM", 10000);
-    //    await hardhatToken.deployed();
-
-    //    const ownerBalance = await hardhatToken.balanceOf(owner.address);
-    //    console.log("Owner Address:", owner.address);
-
-    //    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
-//    });
-//});
