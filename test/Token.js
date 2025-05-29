8fb16c76919d2ca833d2d2777bbacb0ff4af441d
Awais <m.awaisraz786@gmail.com>
2025-05-29 23:29:03 +0500

    passing 13 tests in token.sol

--- test/Token.js
+++ test/Token.js
@@ -23,11 +23,11 @@ describe("Token Contract", function () {
         expect(ownerBalance).to.equal(totalSupply);
     });
 
-    it("should transfer tokens between accounts", async function () { 
-        await token.transfer(addr1.address, ethers.utils.parseUnits("100",18));
+    it("should transfer tokens between accounts", async function () {
+        await token.transfer(addr1.address, ethers.utils.parseUnits("100", 18));
         const addr1Balance = await token.balanceOf(addr1.address);
 
-        expect (addr1Balance).to.equal(ethers.utils.parseUnits("100",18));
+        expect(addr1Balance).to.equal(ethers.utils.parseUnits("100", 18));
     });
 
     it("should fail if the sender doesn't have enough tokens", async function () {
@@ -63,6 +63,39 @@ describe("Token Contract", function () {
         await expect(token.connect(addr1).transferFrom(owner.address, addr2.address, 1)).to.be.revertedWith("You are trying to send more than you allowed.");
     });
 
+    it("should mint tokens only by owner", async function () {
+        const initialSup = await token.totalSupply();
+        const amount = ethers.utils.parseUnits("100", 18);
+        await token.mint(addr1.address, amount);
+        expect(await token.balanceOf(addr1.address)).to.equal(amount);
+        expect(await token.totalSupply()).to.equal(initialSup.add(amount));
+    });
 
+    it("should not mint by non-owner", async function () {
+        await expect(token.connect(addr1).mint(addr1.address, 100)).to.be.revertedWith("Only the owner can call this function.");
+    });
+
+    it("should burn tokens correctly", async function () {
+        const initialSupply = await token.totalSupply();
+        const burnAmount = ethers.utils.parseUnits("100", 18);
+        await token.transfer(addr1.address, burnAmount);
+        await token.connect(addr1).burn(burnAmount);
+        expect(await token.balanceOf(addr1.address)).to.equal(0);
+        expect(await token.totalSupply()).to.equal(initialSupply.sub(burnAmount));
+    });
+
+    it("should pause and unpause transfers", async function () {
+        await token.pause();
+        await expect(token.transfer(addr1.address,1)).to.be.revertedWith("Transfers are paused temporarily.");
+        await token.unpause();
+        await token.transfer(addr1.address,1);
+        expect (await token.balanceOf(addr1.address)).to.equal(1);
+    });
+
+    it("should only allow owner to pause/unpause",async function(){
+        await expect(token.connect(addr1).pause()).to.be.revertedWith("Only the owner can call this function");
+        await token.pause();
+        await expect(token.connect(addr1).unpause()).to.be.revertedWith("Only the owner can call this function");
+    });
 
 });
